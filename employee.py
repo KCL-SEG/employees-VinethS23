938bbac730c8456bc263ba9eaffb8d378a8868dc
Vineth Siriwardana <vineth.siriwardana@kcl.ac.uk>
2023-10-14 22:27:04 +0100

    Created the __str__() function in the Employee class

--- employee.py
+++ employee.py
@@ -29,17 +29,31 @@ class Employee:
         print(self.name + ": " + str(total))
         return total
 
-    def __str__(self): # TO DO
-        return self.name
+    def __str__(self): 
+        printString = self.name + " works on a " + self.contract.contractType + " of "  
+        if self.contract.contractType == "contract":
+            printString = printString + str(self.contract.hours) + " hours at " + str(self.contract.rate) + "/hour"
+        else:
+            printString = printString + str(self.contract.salary)
+        
+        if self.commission == None:
+            pass
+        elif self.commission.commissionType == "Contractly":
+            printString = printString + " and receives a commission for " + str(self.commission.contracts) + " contract(s) at " + str(self.commission.commission) + "/contract."
+        elif self.commission.commissionType == "Bonus":
+            printString = printString + " and receives a bonus commission of " + str(self.commission.bonus)
 
+        printString += ". Their total pay is " + str(self.get_pay()) + "."
+        print(printString)
+        return printString
 ################## CONTRACT TYPES ##################
 
 class Contract:
     def __init__(self):
-        self.a = 21
+        pass
     
 class HourlyContract(Contract):
-    contractType = "Hourly"
+    contractType = "contract"
     def __init__(self, rate, hours):
         super().__init__()
         
@@ -50,9 +64,7 @@ class HourlyContract(Contract):
         return self.rate * self.hours
 
 class MonthlyContract(Contract):
-
-    contractType = "Monthly"
-
+    contractType = "monthly salary"
     def __init__(self, salary):
         super().__init__()
         
@@ -72,6 +84,7 @@ class Commission:
         return self.commission
 
 class ContractCommission(Commission):
+    commissionType = "Contractly"
     def __init__(self, commission, contracts):
         super().__init__(commission)
         self.contracts = contracts
@@ -80,6 +93,7 @@ class ContractCommission(Commission):
         return self.commission * self.contracts
 
 class BonusCommission(Commission):
+    commissionType = "Bonus"
     def __init__(self, commission):
         super().__init__(commission)
 
@@ -89,6 +103,7 @@ class BonusCommission(Commission):
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
 billie = Employee('Billie', MonthlyContract(4000), None)
 billie.get_pay()
+billie.__str__()
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
 charlie = Employee('Charlie', HourlyContract(25, 100), None)
@@ -109,3 +124,4 @@ robbie.get_pay()
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = Employee('Ariel', HourlyContract(30, 120), BonusCommission(600))
 ariel.get_pay()
+billie.__str__()
